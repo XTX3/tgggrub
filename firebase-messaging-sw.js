dcf37ff802fe9a516ff95e51a5808aa5267bc7ec
XTX3 <fro.kurdish@gmail.com>
2026-02-14 17:38:35 +0300

    firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDPcyxpHRuLx9T2CO4LJ-n9jaqAynUjvqw",
+  authDomain: "xix5-a119a.firebaseapp.com",
+  projectId: "xix5-a119a",
+  messagingSenderId: "332265425367",
+  appId: "1:332265425367:web:0a95e71d8bd0058503a392"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload)=>{
+  self.registration.showNotification(payload.notification.title,{
+    body: payload.notification.body
+  });
+});
